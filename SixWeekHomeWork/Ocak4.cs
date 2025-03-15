8c34ad38e5d60737a1ababb2001b37d44dd0359a
sahan <101819553+omerfshan@users.noreply.github.com>
2025-03-15 13:35:25 +0300

    Add initial project files and configuration for SixWeekHomeWork

---
+++ SixWeekHomeWork/Ocak4.cs
@@ -0,0 +1,23 @@
+﻿/*Döngüler, belirli bir koşul sağlandığı sürece kodun tekrar çalıştırılmasını sağlar. Programlamada en yaygın kullanılan döngü türleri for ve while döngüleridir.
+
+For Döngüsü: Belirli bir başlangıç, bitiş ve artış miktarı olduğu durumlarda kullanılır. Genellikle belirli bir sayıda tekrarlanması gereken işlemler için tercih edilir.
+While Döngüsü: Koşul sağlandığı sürece çalışmaya devam eder. Genellikle tekrar sayısının baştan bilinmediği veya belirli bir koşul sağlanana kadar devam etmesi gereken işlemler için kullanılır.*/
+class Ocak4{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.Write("Bir sayı giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        int toplam = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($" birden {n}'e kadar olan sayıların toplamı: {toplam}");
+    }
+}
